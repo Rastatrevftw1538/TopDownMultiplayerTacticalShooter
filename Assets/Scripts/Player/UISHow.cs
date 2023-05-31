c5496e594162b751b54489bb01aad4837df9af81
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2023-05-31 09:55:52 -0700

    Better camera system for easier aiming and better spawning of assets

--- Assets/Scripts/Player/UISHow.cs
+++ Assets/Scripts/Player/UISHow.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class UISHow : NetworkBehaviour
+public class UiShow : NetworkBehaviour
 {
     public Transform target; // Reference to the character's transform
-    public GameObject cameraHolder;
+    public GameObject cameraHolderPrefab;
+
+    private GameObject cameraHolder;
     public GameObject internalUI;
 
     public override void OnStartAuthority()
     {
+        cameraHolder = Instantiate(cameraHolderPrefab,this.transform.parent);
         cameraHolder.SetActive(true);
         internalUI.SetActive(true);
     }
     private void Update()
     {
         cameraHolder.transform.position = transform.position;
-        cameraHolder.transform.rotation = transform.rotation;
-        // Make the camera look at the character's position, but only change its rotation along the Y-axis
-        //Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
-
         //transform.LookAt(targetPosition);
     }
 }
