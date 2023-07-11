ec25b9e6f8b236aef8b26eea46c4c8ef64dea87d
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2023-07-10 22:36:21 -0400

    for matt

---
+++ Assets/Scripts/Managers/GameEvents.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using EvtSystem;
+
+#region Possible Future Dialogue System
+//DIALOGUE SYSTEM STUFF FOR POSSIBLE FUTURE IMPLEMENTATION
+/*
+    public class ShowDialougeText : EvtSystem.Event
+{
+    public string text;
+    public CharacterID id;
+    public float duration;
+}
+
+ public struct ResponseData
+{
+    public string text;
+    public int karmaScore;
+
+    public UnityAction buttonAction;
+}
+
+public class ShowResponses : EvtSystem.Event
+{
+    public ResponseData[] responses; 
+}
+
+ 
+ */
+#endregion
+public class PlayAudio : EvtSystem.Event
+{
+    public AudioClip clipToPlay;
+    public bool isPriority;
+}
+
+public class DisableUI : EvtSystem.Event
+{
+
+}
+
+public class PlayerInteract : EvtSystem.Event
+{
+    public Vector3 interactPosition;
+    public Vector3 interactDirection;
+    public float interactDistance;
+}
+
+public class MyEvent : EvtSystem.Event
+{
+    public int i;
+}
+
+public class CursorMovement : EvtSystem.Event
+{
+    public bool canMove;
+    public float lookSpeed;
+    public CursorLockMode lockMode;
+}
+
+public class FreezePlayerMovement : EvtSystem.Event
+{
+    public bool canMove;
+    public float moveSpeed;
+}
