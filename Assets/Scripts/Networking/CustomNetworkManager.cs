e367c31699ac997e9411f15cababbe9f5c0ad59f
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2023-07-22 17:56:23 -0700

    Working on local networking stuff

--- Assets/Scripts/Networking/CustomNetworkManager.cs
+++ Assets/Scripts/Networking/CustomNetworkManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using System.Net.Sockets;
 
 public class CustomNetworkManager : NetworkManager
 {
@@ -27,33 +28,18 @@ public class CustomNetworkManager : NetworkManager
     }
     public string GetLocalIPAddress()
     {
-        // Get all network interfaces of the device
-        System.Net.NetworkInformation.NetworkInterface[] networkInterfaces =
-            System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+            string localHost = Dns.GetHostName();
+            IPHostEntry hostEntry = Dns.GetHostEntry(localHost);
 
-        // Find the first network interface with an IPv4 address
-        foreach (System.Net.NetworkInformation.NetworkInterface networkInterface in networkInterfaces)
-        {
-            // Consider only operational and non-loopback interfaces
-            if (networkInterface.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up &&
-                networkInterface.NetworkInterfaceType != System.Net.NetworkInformation.NetworkInterfaceType.Loopback)
+            foreach (IPAddress ipAddress in hostEntry.AddressList)
             {
-                // Get the IP properties of the network interface
-                System.Net.NetworkInformation.IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
-
-                // Find the first IPv4 address assigned to the network interface
-                foreach (System.Net.NetworkInformation.UnicastIPAddressInformation unicastAddress in ipProperties.UnicastAddresses)
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    if (unicastAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                    {
-                        return unicastAddress.Address.ToString();
-                    }
+                    return ipAddress.ToString();
                 }
             }
-        }
-
-        // If no suitable IP address is found, return a default value (localhost)
-        return "127.0.0.1";
+            // If no suitable IP address is found, return a default value (localhost)
+            return "127.0.0.1";
     }
     private void SearchForGameManager() {
         Scene gameScene = SceneManager.GetSceneByName("LevelTemplate");
--- Assets/Scripts/StartMenu/StartMenu.cs
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -6,6 +6,9 @@ using Mirror;
 using UnityEngine.SceneManagement;
 using Mirror.Discovery;
 using TMPro;
+using System;
+using kcp2k;
+
 public class StartMenu : MonoBehaviour
 {
     readonly Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
@@ -13,6 +16,8 @@ public class StartMenu : MonoBehaviour
     private NetworkManager networkManager;
     private NetworkDiscovery networkDiscovery;
     private TMP_Text loadingText;
+    public TMP_Text debugText;
+    private string uri;
 
     private void Start() {
         loadingText = GameObject.Find("Searching For Game").GetComponent<TMP_Text>();
@@ -26,6 +31,7 @@ public class StartMenu : MonoBehaviour
         //UnityEditor.Events.UnityEventTools.AddPersistentListener(networkDiscovery.OnServerFound, OnDiscoveredServer);
     }
     private void Update() {
+        debugText.text = uri;
         //Debug.Log("Servers Found = " + discoveredServers.Count);
     }
     public void StartGame()
@@ -39,44 +45,94 @@ public class StartMenu : MonoBehaviour
     }
     private IEnumerator WaitForServerAndStart()
     {
-        float startTime = Time.time;
+        int startRange = 1; // Starting range for the IP address (xxx)
+        int endRange = 254; // Ending range for the IP address (xxx)
+        int portStart = 7777; // Starting port range
+        int portEnd = 7799; // Ending port range
+
         string loadingBar = ".";
-        while (Time.time - startTime < 5f)
+        loadingText.gameObject.SetActive(true);
+
+        // Get the subnet of the device
+        string subnet = GetSubnet();
+
+        for (int endPoint = startRange; endPoint <= endRange; endPoint++)
         {
-            loadingText.gameObject.SetActive(true);
-            if(loadingBar.Length>3){
-                loadingBar = ".";
-            }
-            else{
-                loadingBar+=loadingBar;
-            }
-            loadingBar+=loadingBar;
-            loadingText.text = "Looking For Game\n"+(loadingBar);
-            if (discoveredServers.Count >= 1)
+            for (int port = portStart; port <= portEnd; port++)
             {
-                // Found a server, stop discovery and connect to it
-                networkDiscovery.StopDiscovery();
-                loadingBar="Starting Game";
-                foreach (long serverKey in discoveredServers.Keys)
+                string ipAddress = subnet + endPoint;
+                string uriString = "kcp://" + ipAddress + ":" + port;
+                loadingText.text = "Looking For Game\n" + loadingBar;
+
+                // Try to convert the URI string to a URI object
+                Uri uri = null;
+                if (Uri.TryCreate(uriString, UriKind.Absolute, out uri))
                 {
-                    Debug.Log(serverKey);
-                    Debug.Log(discoveredServers[serverKey].uri);
-                    networkManager.StartClient(discoveredServers[serverKey].uri);
-                    break;
+                    // Try to connect to the server at the current IP address and port using KCP transport
+                    NetworkManager.singleton.StartClient(uri);
+
+                    // Wait for a short time to give the client a chance to connect
+                    yield return new WaitForSeconds(1f);
+
+                    // Check if the client successfully connected
+                    if (NetworkClient.isConnected)
+                    {
+                        // Connected to the server, stop discovery
+                        networkDiscovery.StopDiscovery();
+                        loadingText.text = "Connected to Server";
+                        yield break; // Exit the coroutine
+                    }
+                    else
+                    {
+                        // Connection attempt failed, disconnect and try the next port
+                        networkManager.StopClient();
+                    }
                 }
-                yield break; // Exit the coroutine early if a server is found
             }
-            yield return null; // Wait for the next frame
         }
 
         // No server found, start a host
-        loadingBar="Starting Game";
-        Debug.Log("No server found, launching Server on Client");
+        loadingText.text = "No server found, starting Server on Client";
         networkDiscovery.BroadcastAddress = CustomNetworkManager.singleton.networkAddress;
         networkDiscovery.StopDiscovery();
         CustomNetworkManager.singleton.StartHost();
         networkDiscovery.AdvertiseServer();
     }
+
+    private string GetSubnet()
+    {
+        // Get all network interfaces of the device
+        System.Net.NetworkInformation.NetworkInterface[] networkInterfaces =
+            System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+
+        // Find the first network interface with an IPv4 address
+        foreach (System.Net.NetworkInformation.NetworkInterface networkInterface in networkInterfaces)
+        {
+            // Consider only operational and non-loopback interfaces
+            if (networkInterface.OperationalStatus == System.Net.NetworkInformation.OperationalStatus.Up &&
+                networkInterface.NetworkInterfaceType != System.Net.NetworkInformation.NetworkInterfaceType.Loopback)
+            {
+                // Get the IP properties of the network interface
+                System.Net.NetworkInformation.IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+
+                // Find the first IPv4 address assigned to the network interface
+                foreach (System.Net.NetworkInformation.UnicastIPAddressInformation unicastAddress in ipProperties.UnicastAddresses)
+                {
+                    if (unicastAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        string ipAddress = unicastAddress.Address.ToString();
+                        string[] parts = ipAddress.Split('.');
+                        // Return the subnet (first three parts of the IP address)
+                        return parts[0] + "." + parts[1] + "." + parts[2] + ".";
+                    }
+                }
+            }
+        }
+
+        // If no suitable IP address is found, return a default value ("10.1.0.")
+        return "127.0.0.";
+    }
+
     public void OnDiscoveredServer(ServerResponse info)
         {
             // Note that you can check the versioning to decide if you can connect to the server or not using this method
