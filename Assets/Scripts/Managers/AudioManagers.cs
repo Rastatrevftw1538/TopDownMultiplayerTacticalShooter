c1ee53748569c3d2614181d48e0db8b2a513ca22
Ryan R <33332787+RyannR9375@users.noreply.github.com>
2023-08-24 20:44:46 -0700

    Option to add more than one gun sound (janky)
    tune "AudioManagers.cs" i know i messed up the file name ALL GOOD

--- Assets/Scripts/Managers/AudioManagers.cs
+++ Assets/Scripts/Managers/AudioManagers.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManagers : MonoBehaviour
 {
     [Header("Bullet Sounds")]
     public AudioClip ARSound;
+    public AudioClip ARSound2;
     public AudioClip PistolSound;
     public AudioClip SMGSound;
     public AudioClip ShotgunSound;
@@ -29,22 +31,33 @@ public class AudioManagers : MonoBehaviour
 
     private void PlayShootSound(ShootSound evtData)
     {
+        audioSource.transform.position = evtData.position;
+        //int rand = Random.Range(0, 2);
+        //Debug.LogError("random: " + rand);
         switch (evtData.GunName)
         {
             case "AR":
-                AudioSource.PlayClipAtPoint(ARSound, evtData.position);
+                //AudioSource.PlayClipAtPoint(ARSound, evtData.position);
+                //if (rand == 0)
+                    audioSource.PlayOneShot(ARSound);
+                //else
+                //    audioSource.PlayOneShot(ARSound2);
                 break;
             case "Pistol":
-                AudioSource.PlayClipAtPoint(PistolSound, evtData.position);
+                //AudioSource.PlayClipAtPoint(PistolSound, evtData.position);
+                audioSource.PlayOneShot(PistolSound);
                 break;
             case "SMG":
-                AudioSource.PlayClipAtPoint(SMGSound, evtData.position);
+                //AudioSource.PlayClipAtPoint(SMGSound, evtData.position);
+                audioSource.PlayOneShot(SMGSound);
                 break;
             case "Shotgun":
-                AudioSource.PlayClipAtPoint(ShotgunSound, evtData.position, 0.3f);
+                //AudioSource.PlayClipAtPoint(ShotgunSound, evtData.position, 0.3f);
+                audioSource.PlayOneShot(ShotgunSound, 0.3f);
                 break;
             case "Sniper":
-                AudioSource.PlayClipAtPoint(SniperSound, evtData.position);
+                //AudioSource.PlayClipAtPoint(SniperSound, evtData.position);
+                audioSource.PlayOneShot(SniperSound);
                 break;
         }
     }
