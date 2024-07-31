cf341e8701e0e73015c49dcf72764a937b79636c
Ryan R <ryannr9375@gmail.com>
2024-07-31 16:34:12 -0400

    sprites on enemies

--- Assets/Scripts/Weapon/SinglePlayer/WeaponSinglePlayer.cs
+++ Assets/Scripts/Weapon/SinglePlayer/WeaponSinglePlayer.cs
@@ -398,7 +398,7 @@ public class WeaponSinglePlayer : MonoBehaviour
         Destroy(tempParticle, 0.5f);
 
         //IF ON BEAT, MAKE THE TRAIL RENDER DIFFERENT COLOR
-        if (onBeat)
+        if (CheckBPM())
         {
             TrailRenderer trailrenderer = trailRender.GetComponent<TrailRenderer>();
 
@@ -410,7 +410,6 @@ public class WeaponSinglePlayer : MonoBehaviour
             }
 
             //some camera shake stuff (unoptimized)
-
             //camera shake
             StartCoroutine(ClientCamera.Instance.cameraShake.CustomCameraShake(0.1f, 0.2f));
         }
