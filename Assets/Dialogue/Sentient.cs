f0e1a7e9c3631077d8c50400b4c3fa5f7f5dd13f
yenbad <stilldabz@gmail.com>
2024-08-01 16:04:40 -0700

    made placeholder for IInteractable in Sentient class

--- Assets/Dialogue/Sentient.cs
+++ Assets/Dialogue/Sentient.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Sentient : MonoBehaviour
+public abstract class Sentient : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if//(eventsystemTrigger == true)
+        {
+            //interact
+        }
     }
 }
