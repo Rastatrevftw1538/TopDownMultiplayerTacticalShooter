f82f409b7bf83782810264b6c6fb5ad7433b3662
yenbad <stilldabz@gmail.com>
2024-08-01 14:58:21 -0700

    dialogueText script - added menu asset, read in  speaker name, type, text

--- Assets/Dialogue/dialogueText.cs
+++ Assets/Dialogue/dialogueText.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dialogueText : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(menuName = "Dialogue/New Dialouge - Container")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+public class dialogueText : ScriptableObject
+{
+    public string speakerName;
+    public string speakerType;
+
+    public string[] paragraphs;
+} 
