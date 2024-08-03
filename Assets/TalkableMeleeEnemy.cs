9bba802960bdc10f8676cdb746d5bc7d0545f519
yenbad <stilldabz@gmail.com>
2024-08-03 09:39:32 -0700

    added interact icon to talkable melee enemy in scene, called talk function in TalkableMeleeEnemy class
    ran into an issue where the TalkableMeleeEnemy script isn't inheriting from monobehaviour which makes me think it isn't correctly inheriting the Sentient class

--- Assets/TalkableMeleeEnemy.cs
+++ Assets/TalkableMeleeEnemy.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class TalkableMeleeEnemy : Sentient, ITalkable
 {
-    //variable to read in the text its supposed to say
+
     [SerializeField] private dialogueText DialogueText;
-
-    public override void Interact()
-    {
-        Talk(DialogueText);
-    }
-
 
     public void Talk(dialogueText DialogueText)
     {
-        //start conversation
+
+    }
+
+    public override void Interact()
+    {
+
     }
 }
-
